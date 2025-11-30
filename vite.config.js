6dd2d76804ff6249b7a3c9d8881e9cf52aafe760
rifaldo <muhammadrifaldosaputra@gmail.com>
2025-11-30 10:46:07 +0700

    feat: update layout and styles for kelas and materi pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    lib: false,
-  },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        materi: resolve(__dirname, 'materi.html'),
+        kelas: resolve(__dirname, 'kelas.html'),
+      }
+    }
+  }
 })
